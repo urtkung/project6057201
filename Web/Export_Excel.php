de48b34c8081f6533fbf281936c46809d819bd30
urtkung <63046741+urtkung@users.noreply.github.com>
2020-11-16 19:50:17 +0700

    Update Export_Excel.php

--- Web/Export_Excel.php
+++ Web/Export_Excel.php
@@ -64,50 +64,27 @@ if(isset($_POST["To_Excel"])){
         $Finger_sel = $_POST['fing_sel'];
         $_SESSION['searchQuery'] .= " AND fingerprint_id='".$Finger_sel."'";
     }
-    //Department filter
-    if ($_POST['dev_id'] != 0) {
-        $dev_id = $_POST['dev_id'];
-        $sql = "SELECT device_uid FROM devices WHERE id=?";
-        $result = mysqli_stmt_init($conn);
-        if (!mysqli_stmt_prepare($result, $sql)) {
-            echo "SQL_Error";
-            exit();
-        }
-        else{
-            mysqli_stmt_bind_param($result, "i", $dev_id);
-            mysqli_stmt_execute($result);
-            $resultl = mysqli_stmt_get_result($result);
-            if ($row = mysqli_fetch_assoc($resultl)) {
-                $dev_uid = $row['device_uid'];
-            }
-        }
-        $_SESSION['searchQuery'] .= " AND device_uid='".$dev_uid."'";
-    }
 
-    $sql = "SELECT * FROM users_logs WHERE ".$_SESSION['searchQuery']." ORDER BY id DESC";
+
+    $sql = "SELECT * FROM users_logs WHERE ".$_SESSION['searchQuery']." ORDER BY timein DESC";
     $result = mysqli_query($conn, $sql);
     if($result->num_rows > 0){
-      $output .= '
+      $output .= 'ประวัติการเข้าร่วมกิจกรรม
                   <table class="table" bordered="1">  
                     <TR>
-                      <TH>ID</TH>
-                      <TH>Name</TH>
-                      <TH>Serial Number</TH>
-                      <TH>Fingerprint ID</TH>
-                      <TH>Device ID</TH>
-                      <TH>Device Dep</TH>
-                      <TH>Date log</TH>
-                      <TH>Time In</TH>
-                      <TH>Time Out</TH>
+                      <TH>ชื่อ</TH>
+                      <TH>รหัสนักศึกษา</TH>
+                      <TH>ชื่อกิจกรรม</TH>
+                      <TH>วันที่เข้าร่วม</TH>
+                      <TH>เวลาเข้า</TH>
+                      <TH>เวลาออก</TH>
                     </TR>';
         while($row=$result->fetch_assoc()) {
             $output .= '
                         <TR> 
-                            <TD> '.$row['id'].'</TD>
+
                             <TD> '.$row['username'].'</TD>
                             <TD> '.$row['serialnumber'].'</TD>
-                            <TD> '.$row['fingerprint_id'].'</TD>
-                            <TD> '.$row['device_uid'].'</TD>
                             <TD> '.$row['device_dep'].'</TD>
                             <TD> '.$row['checkindate'].'</TD>
                             <TD> '.$row['timein'].'</TD>
